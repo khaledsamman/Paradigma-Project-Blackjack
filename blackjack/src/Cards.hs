01fe5f32162914d56a8896327ba03db42bd6acf4
Khaled El Samman <k.elsamman@student.han.nl>
2025-09-15 18:37:03 +0200

    Update Cards.hs

--- blackjack/src/Cards.hs
+++ blackjack/src/Cards.hs
@@ -16,6 +16,8 @@ data Card = Card CardValue CardSuite deriving (Eq)
 
 -- synonym for list of cards to store decks
 type Deck = [Card]
+type Hand = [Card]
+
 
 instance Show CardValue where
   show c = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"] !! fromEnum c
@@ -38,3 +40,42 @@ dealCards :: Int -> Deck -> (Deck, Deck)
 dealCards n deck = (drop n deck, take n deck)
 countCards :: Deck -> (Int, Deck)
 countCards deck = (length deck, deck)
+
+-- state for the player
+data Player = Player {
+    playerHand :: Hand,
+    playerBet :: Int,
+    playerMoney :: Int
+} deriving (Show)
+
+--state for the dealer
+data Dealer = Dealer {
+    dealerHand :: Hand
+} deriving (Show)
+
+--functions that extract rank and suit from a card
+rank :: Card -> CardValue
+rank (Card r _) = r
+suit :: Card -> CardSuite
+suit (Card _ s) = s
+
+
+--calculate the blackjack value of a card, Ace is tricky!
+cardValue :: Card -> Int
+cardValue (Card rank _) = case rank of
+  Ace   -> 11 -- We'll handle Ace's 1 or 11 value in handValue
+  Jack  -> 10
+  Queen -> 10
+  King  -> 10
+  _     -> fromEnum rank - fromEnum Two + 2
+
+-- calculate the value of a hand
+handValue :: Hand -> Int
+handValue hand =
+  let base = sum (map cardValue hand)                 -- Ace=11 initially
+      nAces = length (filter (\c -> rank c == Ace) hand)
+  in adjust base nAces
+  where
+    adjust total a
+      | total > 21 && a > 0 = adjust (total - 10) (a - 1)
+      | otherwise           = total
