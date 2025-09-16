8e1d8d3cf4b7a2424dc5ba93ece4dfada0db2576
Khaled El Samman <k.elsamman@student.han.nl>
2025-09-16 11:29:04 +0200

    Update GameFloor.hs

--- blackjack/src/GameFloor.hs
+++ blackjack/src/GameFloor.hs
@@ -28,3 +28,74 @@ playerBetAction = do
   p0  <- gets player
   let p1 = makeBet bet p0
   modify (\s -> s { player = p1 })
+
+
+
+dealCardsToPlayer :: Int -> Game ()
+dealCardsToPlayer n = do
+    -- get current game state
+    gs <- get
+    let pl = player gs
+    -- deal cards using pure function
+    let (newDeck, playerDealt) = dealCards n (deck gs)
+    -- new player state with new cards
+    let pl' = pl { playerHand = (playerHand pl) ++ playerDealt }
+    -- update game state
+    modify (\s -> s { player = pl', deck = newDeck })
+
+
+showPlayerState :: Game ()
+showPlayerState = do
+    pl <- gets player
+    liftIO $ print pl
+
+-- dealer takes cards
+dealerAction :: Int -> Game ()
+dealerAction pvalue = do
+    dl <- gets dealer
+    let value = handValue (dealerHand dl)
+    if value < 17
+        then do
+            dealCardsToDealer 1
+            showDealerHand
+            dealerAction pvalue
+        else do
+            showDealerHand
+            liftIO $ putStrLn $ "Dealer stands with value: " ++ show value
+            if ((value > 21) || (value < pvalue))
+                then do
+                    liftIO $ putStrLn "Player wins!"
+                    pl <- gets player
+                    let pl' = payout ((playerBet pl)*3) pl
+                    modify (\s -> s { player = pl' })
+                    showPlayerState
+                else do
+                    liftIO $ putStrLn "Player Looses :("
+                    pl <- gets player
+                    let pl' = payout 0 pl
+                    modify (\s -> s { player = pl' })
+                    showPlayerState
+
+-- main player round
+playerAction :: Game ()
+playerAction = do
+    showDealerHand
+    showPlayerState
+    action <- liftIO $ do
+        putStrLn "Choose action: (h)it or (s)tand"
+        getLine
+    case action of 
+        "H" -> do
+            dealCardsToPlayer 1
+            pl <- gets player
+            let newValue = handValue (playerHand pl)
+            if newValue > 21
+                then do
+                    liftIO $ putStrLn "Player busts! Dealer wins :("
+                    showPlayerState
+                    let pl'' = payout 0 pl'
+                    modify (\s -> s { player = pl'' })
+                    showPlayerState
+                    -- modifyPlayer 
+    
+                
