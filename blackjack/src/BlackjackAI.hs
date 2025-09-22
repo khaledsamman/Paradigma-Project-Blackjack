5f5427858da36dcdf4cb0f2d31ec66e729843777
Khaled El Samman <k.elsamman@student.han.nl>
2025-09-22 12:37:36 +0200

    Update BlackjackAI.hs

--- blackjack/src/BlackjackAI.hs
+++ blackjack/src/BlackjackAI.hs
@@ -6,7 +6,6 @@ module BlackjackAI
 import Cards 
 
 
-
 --https://www.blackjackapprenticeship.com/how-to-count-cards/
 
 hiLoCard :: Card -> Int
@@ -31,7 +30,8 @@ runningCount :: [Card] -> Int
 runningCount = sum . map hiLoCard
 
 
--- casinos use up to 8 decks in blackjack, so only fair if we do the same xd
+-- casinos use up to 8 decks in blackjack, so it's only fair if we do the same xd
+
 -- To turn a running count into a true count, you divide by decks remaining. This function gives you that divisor (e.g., 26 cards left → 0.5 decks).
 remainingDecks :: Deck -> Double
 remainingDecks d =
@@ -40,3 +40,11 @@ remainingDecks d =
 
 trueCount :: [Card] -> Deck -> Double
 trueCount seen d = fromIntegral (runningCount seen) / remainingDecks d
+
+-- based on true count, decide whether to bet high or low
+shouldBetHigh :: [Card] -> Deck -> Bool
+shouldBetHigh seen d = trueCount seen d >= 2.0
+shouldBetLow :: [Card] -> Deck -> Bool
+shouldBetLow seen d = trueCount seen d < 2.0
+shouldBet :: [Card] -> Deck -> Int
+shouldBet seen d = if shouldBetHigh seen d then 100 else 10
